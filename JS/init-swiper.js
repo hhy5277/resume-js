698b20e25843316b6ae255fdcc6f0e3ffe630b96
lynchuh <632955934@qq.com>
2018-06-01 19:42:43 +0800

    按模块建立js文件

---
+++ JS/init-swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  });
---
+++ JS/sticky-topBar.js
@@ -0,0 +1,4 @@
+		window.onscroll = function (){
+			window.scrollY !== 0 ? $(navBar).addClass('sticky'):$(navBar).removeClass('sticky')
+        }
+        //submenuSetting

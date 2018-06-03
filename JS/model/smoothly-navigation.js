edede682fa90142cd010181e2e8172b50f5fe37c
lynchuh <632955934@qq.com>
2018-06-03 14:55:31 +0800

    改一点navigation的小bug

--- JS/model/smoothly-navigation.js
+++ JS/model/smoothly-navigation.js
@@ -7,7 +7,6 @@
         minIndex:null,
         init: function(view){
             this.view = view
-            this.deactiveTag()
             this.activeTag()
             this.bindEvent()
             console.log('1')
@@ -20,6 +19,7 @@
                 {
                     this.minIndex = i
                 }
+                this.deactiveTag()
                 this.highlightTag = document.querySelector('a[href="#' + this.view[this.minIndex].id +'"]' )
                 this.highlightTag.parentElement.classList.add('highlight')
                 this.view[this.minIndex].classList.remove('offset')	
@@ -34,7 +34,6 @@
         },
         bindEvent: function(){
             window.addEventListener('scroll',()=>{
-                this.deactiveTag()
                 this.activeTag()
                 console.log(4)
             })
@@ -43,6 +42,27 @@
     
     controller.init(view)
 
-    
+    // xx() 
+    // window.addEventListener('scroll',xx)
+    // function xx(){
+    //     let specialTags = document.querySelectorAll('[data-x]')
+    //     let minIndex = 0
+    //     specialTags[minIndex].classList.remove('offset')	
+    //     for(let i=1; i < specialTags.length; i++){
+    //         if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop-window.scrollY))
+    //         {
+    //             minIndex = i
+    //         }
+    //         let id = specialTags[minIndex].id
+    //         let highlightTag = document.querySelector('a[href="#' + id +'"]' )
+    //         for(let i = 0;i<specialTags.length;i++){
+    //             let highlightTags = document.querySelector('a[href="#' +specialTags[i].id+'"]')
+    //             highlightTags.parentElement.classList.remove('highlight')
+    //             specialTags[i].classList.add('offset')
+    //         }
+    //         highlightTag.parentElement.classList.add('highlight')
+    //         specialTags[minIndex].classList.remove('offset')			
+    //     }
+    // }
 
 }.call()
